53d73f0edb33e4bad3478e48ae3f6a290aa049e3
sakulstra <lukasstrassel@googlemail.com>
2021-04-02 11:52:46 +0200

    fix: add editorconfig for cleaner diff

--- scripts/test-liquiditySwap-swapAndDeposit-swapAll-WETH-DAI.js
+++ scripts/test-liquiditySwap-swapAndDeposit-swapAll-WETH-DAI.js
@@ -1,56 +1,56 @@
-require("dotenv").config();
-const { ethers } = require("ethers");
-const { ParaSwap } = require("paraswap");
+require('dotenv').config();
+const { ethers } = require('ethers');
+const { ParaSwap } = require('paraswap');
 const {
   TxBuilderV2,
   distinctContractAddressBetweenMarketsV2,
-} = require("@aave/protocol-js");
-const { TenderlyFork } = require("../src/tenderly");
-const { WETH, DAI, API_ETH_MOCK_ADDRESS } = require("../src/addresses");
+} = require('@aave/protocol-js');
+const { TenderlyFork } = require('../src/tenderly');
+const { WETH, DAI, API_ETH_MOCK_ADDRESS } = require('../src/addresses');
 
-const IERC20_ARTIFACT = require("../artifacts/IERC20.json");
-const IWETH_ARTIFACT = require("../artifacts/IWETH.json");
-const ILendingPool_ARTIFACT = require("../artifacts/ILendingPool.json");
+const IERC20_ARTIFACT = require('../artifacts/IERC20.json');
+const IWETH_ARTIFACT = require('../artifacts/IWETH.json');
+const ILendingPool_ARTIFACT = require('../artifacts/ILendingPool.json');
 
-const FORK_NETWORK_ID = process.env.FORK_NETWORK_ID || "1";
-const PARASWAP_API = process.env.PARASWAP_API || "https://apiv2.paraswap.io/v2";
+const FORK_NETWORK_ID = process.env.FORK_NETWORK_ID || '1';
+const PARASWAP_API = process.env.PARASWAP_API || 'https://apiv2.paraswap.io/v2';
 
 async function main() {
   // Create a new random account signer
   let signer = ethers.Wallet.createRandom();
-  console.log("Created wallet for address", signer.address);
+  console.log('Created wallet for address', signer.address);
 
   // Build a swap on ParaSwap
-  const amount_to_swap = ethers.utils.parseEther("10.01");
+  const amount_to_swap = ethers.utils.parseEther('10.01');
   const paraswap = new ParaSwap(parseInt(FORK_NETWORK_ID), PARASWAP_API);
   const priceRoute = await paraswap.getRate(
-    "WETH",
-    "DAI",
+    'WETH',
+    'DAI',
     amount_to_swap.toString(),
-    "SELL",
-    { referrer: "aave", excludeDEXS: "Balancer", excludeMPDEXS: "Balancer" }
+    'SELL',
+    { referrer: 'aave', excludeDEXS: 'Balancer', excludeMPDEXS: 'Balancer' }
   );
   const { others, ...priceRouteNoOthers } = priceRoute;
-  console.log("priceRoute:", JSON.stringify(priceRouteNoOthers, null, 2));
-  if (priceRoute.message) throw new Error("Error getting priceRoute");
+  console.log('priceRoute:', JSON.stringify(priceRouteNoOthers, null, 2));
+  if (priceRoute.message) throw new Error('Error getting priceRoute');
   const txParams = await paraswap.buildTx(
-    "WETH",
-    "DAI",
+    'WETH',
+    'DAI',
     priceRoute.srcAmount,
     priceRoute.priceWithSlippage,
     priceRouteNoOthers,
     signer.address,
-    "aave",
+    'aave',
     undefined,
     { ignoreChecks: true, forceMultiSwap: true }
   );
-  console.log("txParams:", txParams);
-  if (txParams.message) throw new Error("Error getting txParams");
+  console.log('txParams:', txParams);
+  if (txParams.message) throw new Error('Error getting txParams');
 
   // Create a fork on Tenderly
   const fork = new TenderlyFork();
   await fork.init();
-  console.log("Created fork ID", fork.fork_id);
+  console.log('Created fork ID', fork.fork_id);
 
   // Connect the signer to the fork
   const provider = new ethers.providers.JsonRpcProvider(
@@ -62,22 +62,22 @@ async function main() {
   // Fund the account with fake ETH
   await fork.fund_account(signer.address);
   console.log(
-    "Balance was set to",
+    'Balance was set to',
     ethers.utils.formatEther(await signer.getBalance()),
-    "ETH"
+    'ETH'
   );
 
   // Initialize the txBuilder
-  const txBuilder = new TxBuilderV2("mainnet", fork.get_rpc_url());
+  const txBuilder = new TxBuilderV2('mainnet', fork.get_rpc_url());
 
   const lending_pool_address =
-    distinctContractAddressBetweenMarketsV2["proto"]["mainnet"]
+    distinctContractAddressBetweenMarketsV2['proto']['mainnet']
       .LENDINGPOOL_ADDRESS;
 
   // Deposit ETH to get WETH
   // I think we should be able to drop this manual wrapping as deposit is capable of handling it - didn't work though & I didn't yet figure out why
-  console.log("Depositing to WETH...");
-  const deposit_amount = ethers.utils.parseEther("10");
+  console.log('Depositing to WETH...');
+  const deposit_amount = ethers.utils.parseEther('10');
   const weth = new ethers.Contract(
     WETH[FORK_NETWORK_ID],
     IWETH_ARTIFACT.abi,
@@ -85,16 +85,16 @@ async function main() {
   );
   await (await weth.deposit({ value: deposit_amount })).wait();
   console.log(
-    "Deposited",
+    'Deposited',
     ethers.utils.formatEther(deposit_amount),
-    "ETH as WETH"
+    'ETH as WETH'
   );
 
   // Deposit WETH in Aave AToken
-  console.log("Depositing to AToken...");
-  const depositTxs = await txBuilder.getLendingPool("proto").deposit({
+  console.log('Depositing to AToken...');
+  const depositTxs = await txBuilder.getLendingPool('proto').deposit({
     reserve: WETH[FORK_NETWORK_ID], // API_ETH_MOCK_ADDRESS
-    amount: "10",
+    amount: '10',
     user: signer.address,
   });
   for (const tx of depositTxs) {
@@ -111,19 +111,19 @@ async function main() {
   const aWETH = new ethers.Contract(aWETH_address, IERC20_ARTIFACT.abi, signer);
   const aWETH_balance_before = await aWETH.balanceOf(signer.address);
   console.log(
-    "Successfully deposited",
+    'Successfully deposited',
     ethers.utils.formatEther(aWETH_balance_before),
-    "WETH in AToken"
+    'WETH in AToken'
   );
 
   // Perform the swap on the adapter
-  console.log("Performing swap using swapCollateral...");
-  const swapTxs = await txBuilder.getLendingPool("proto").swapCollateral({
+  console.log('Performing swap using swapCollateral...');
+  const swapTxs = await txBuilder.getLendingPool('proto').swapCollateral({
     fromAsset: WETH[FORK_NETWORK_ID],
     toAsset: DAI[FORK_NETWORK_ID],
     swapAll: true,
-    fromAToken: "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e",
-    maxSlippage: "2",
+    fromAToken: '0x030bA81f1c18d280636F32af80b9AAd02Cf0854e',
+    maxSlippage: '2',
     fromAmount: ethers.utils.formatUnits(priceRoute.srcAmount, 18),
     toAmount: ethers.utils.formatUnits(priceRoute.priceWithSlippage, 18),
     user: signer.address,
@@ -134,18 +134,18 @@ async function main() {
   for (const tx of swapTxs) {
     await (await signer.sendTransaction(await tx.tx())).wait();
   }
-  console.log("Swap performed successfully!");
+  console.log('Swap performed successfully!');
   const aDAI_address = (await lending_pool.getReserveData(DAI[FORK_NETWORK_ID]))
     .aTokenAddress;
   const aDAI = new ethers.Contract(aDAI_address, IERC20_ARTIFACT.abi, signer);
   const aWETH_balance_after = await aWETH.balanceOf(signer.address);
   const aDAI_balance_after = await aDAI.balanceOf(signer.address);
   console.log(
-    "Final balances",
+    'Final balances',
     ethers.utils.formatEther(aWETH_balance_after),
-    "aWETH",
+    'aWETH',
     ethers.utils.formatUnits(aDAI_balance_after, 18),
-    "aDAI"
+    'aDAI'
   );
 }
 
